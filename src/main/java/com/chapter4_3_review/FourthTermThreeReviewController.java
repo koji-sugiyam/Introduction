d439200d124c34c317cdd5016d003e28fada077e
STARTTODAY\koji.sugiyama <koji.sugiyama@starttoday.jp>
2017-12-18 11:38:15 +0900

    chapter 4-3 ここまでの復習 クラス（インターフェース、抽象クラス、継承など）、出力、ループ、判別、文字列処理イテレーターなど

--- src/main/java/com/chapter4_3_review/FourthTermThreeReviewController.java
+++ src/main/java/com/chapter4_3_review/FourthTermThreeReviewController.java
@@ -31,8 +31,8 @@ public class FourthTermThreeReviewController extends BaseController {
 		//ArrayListの復習
 		//配列に銀行名一覧を作成
 		//自分が好きな銀行名だけを抽出して表示
-		//一度出力確認後、ループして配当銀行を表示。
-		//その後、他の銀行は削除する。ただし、イオン銀行は残してあげようじゃないか。
+		//それ以外の銀行は削除対象としてリスト作成
+		//ループして削除該当銀行を削除する。ただし、イオン銀行は残してあげようじゃないか。
 		//最終結果はブラウザに表示
 		
 		return service.reviewRun();
